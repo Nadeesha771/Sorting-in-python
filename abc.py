e3b016161182247a5e63d209c56db058b874ebde
Nadeesha Lakshan <92171851+Nadeesha771@users.noreply.github.com>
2021-10-08 21:35:37 +0530

    edit sorting

--- abc.py
+++ abc.py
@@ -1,3 +1,4 @@
+#Bubble sort in python
 def bubbleSort(nlist):
     for passnum in range(len(nlist)-1,0,-1):
         for i in range(passnum):
@@ -8,4 +9,48 @@ def bubbleSort(nlist):
 
 nlist = [14,46,43,27,57,41,45,21,70]
 bubbleSort(nlist)
+print(nlist)
+
+
+
+
+
+#Merge sort in python
+
+
+
+def mergeSort(nlist):
+    if len(nlist) > 1:
+        mid = len(nlist) // 2
+        left = nlist[:mid]
+        right = nlist[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = 0
+        j = 0
+        
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              nlist[k] = left[i]
+              i += 1
+            else:
+                nlist[k] = right[j]
+                j += 1
+           
+            k += 1
+            
+        while i < len(left):
+            nlist[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nlist[k]=right[j]
+            j += 1
+            k += 1
+
+nlist = [14,46,43,27,57,41,45,21,70]
+mergeSort(nlist)
 print(nlist)
